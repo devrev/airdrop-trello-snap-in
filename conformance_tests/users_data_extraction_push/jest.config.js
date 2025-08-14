f258f54babe7783298da3627fee3cd81b9ee5759
Tjaz Erzen <tjaz@codeplain.ai>
2025-08-14 14:49:20 +0200

    fixup! Rendered trello snapin

--- conformance_tests/users_data_extraction_push/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testTimeout: 60000,
-  testMatch: ['**/*.test.ts'],
-  setupFilesAfterEnv: ['./jest.setup.js']
-};
--- conformance_tests/users_data_extraction_push/jest.setup.js
+++
@@ -1,2 +0,0 @@
-// This file is managed externally and cannot be modified
-// It's included here for completeness
